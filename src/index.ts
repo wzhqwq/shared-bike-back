9908963db312a52d84c8de01ef97b90f325c851c
wzhqwq <wzhii345@outlook.com>
2022-09-05 07:56:35 +0800

    修改文案

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use((ctx, next) =>
   next().catch(err => {
     if (err.status === 401) {
       ctx.status = 401
-      ctx.body = 'JWT无效'
+      ctx.body = '需要登录'
     }
     else {
       switch (err.name) {
